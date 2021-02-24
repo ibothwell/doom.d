3d5bfa3536a8790968c80e936e57d9df612eeaaf
Ian Bothwell <ibothwell@rockportnetworks.com>
2021-02-24 15:07:08 -0500

    Imported old ido config into new doom config… could not live without symbol lookup.

--- config.el
+++ config.el
@@ -68,6 +68,7 @@
 ;; Config files
 (load! "./config/common-config.el")
 (load! "./config/osx.el")
+(load! "./config/ido-config.el")
 (load! "./config/ui-config.el")
 (load! "./config/editing.el")
 (load! "./config/indent-config.el")
